3d0db680e0ba3e99a2e688a619d40f4923e6d6af
Sunayana Hubli <112987864+sunayanahubli@users.noreply.github.com>
2023-10-24 17:10:15 -0400

    minor updates

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-document.getElementById('dark-mode-switch').addEventListener('change', function() {
-    if (this.checked) {
+document.addEventListener('DOMContentLoaded', function() {
+    // Check if the user has a preference for dark mode
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
     }
+
+    // Toggle dark mode based on the checkbox state
+    document.getElementById('dark-mode-switch').addEventListener('change', function() {
+        if (this.checked) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
 });
